4f9fd2ea84d412dbdf02357ba8de95dd5da04513
Joseph A Streeter <joseph.streeter76@gmail.com>
2025-01-10 19:36:53 -0600

    updated password hash code

--- website/auth.py
+++ website/auth.py
@@ -55,8 +55,8 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            hashed_password = generate_password_hash(password1, method='pbkdf2:sha256', salt_length=16)
+            new_user = User(email=email, first_name=first_name, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
